cb8a2c364b01bd7038c2332fea9bec266c5649e7
AIT MANSOUR Mohamed <mohamedaitmansour95@gmail.com>
2018-04-06 12:29:41 +0200

    New And Edit Contact

--- src/m1/piu/FXMLMainFrameController.java
+++ src/m1/piu/FXMLMainFrameController.java
@@ -1,16 +1,40 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The MIT License
+ *
+ * Copyright 2018 Mohamed AIT MANSOUR <contact@numidea.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package m1.piu;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  *
@@ -19,14 +43,47 @@ import javafx.scene.control.Label;
 public class FXMLMainFrameController implements Initializable {
     
     @FXML
-    private Label label;
-    
+    private Label label;   
+   
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+   private void newContact(ActionEvent event) throws IOException{
+       
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("FXMLNewContact.fxml"));
+            AnchorPane root2 = (AnchorPane) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root2));
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setResizable(false);
+            stage.setTitle("New Contact");
+            stage.showAndWait();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+   
+       
+   }
+   
+   
+     @FXML
+    private void addPhoneNumber(ActionEvent event) throws  IOException{
+       
+         try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("FXMLNewPhoneNumber.fxml"));
+            AnchorPane root1 = (AnchorPane) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setResizable(false);
+            stage.setTitle("New Phone Number");
+            stage.showAndWait();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+      
     }
-    
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
